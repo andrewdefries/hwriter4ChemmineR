8d007fbc70393e34a1dd7f9498ad83efc25276a8
andrewdefries <ajd@backmandefries.com>
2014-04-17 14:59:38 -0700

    fixed the bug lost names via unlist the smiset[[a]]

--- SDFsets/MakeMeChemicalTables.sh
+++ SDFsets/MakeMeChemicalTables.sh
@@ -1,13 +1,31 @@
+rm -r WorkHere
+
 mkdir WorkHere
 
 cp *.R WorkHere/
 
-cp *Cleaned.sdf WorkHere/
+cp YAWYE.sdf WorkHere/
 
 cd WorkHere/
+
+echo "####################"
+echo "####################"
+echo "####################"
+echo "ChemmineR and OpenBabel are working on it, have a large coffee if your file is bigger than 1000 compounds"
+echo "####################"
+echo "####################"
+echo "####################"
 
 R CMD BATCH CleanSDF.R
 
 R CMD BATCH hwriteMoreNow.R
+
+echo "####################"
+echo "####################"
+echo "####################"
+echo "Work is done, please inspect test.html for accuracy"
+echo "####################"
+echo "####################"
+echo "####################"
 
 firefox test.html
